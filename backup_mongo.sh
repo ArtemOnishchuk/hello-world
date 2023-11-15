8726bc63ac72275498c040595b4c92cfbcd1ee01
ArtemOnishchuk <belarys1@mail.ru>
2023-11-15 11:27:06 +0300

    Update backup_mongo.sh

--- backup_mongo.sh
+++ backup_mongo.sh
@@ -1,3 +1,6 @@
 kubectl exec -n mongodb mongodb-0 -- \
 	bash -c 'gunzip -c /tmp/*.gz | mongorestore --archive -u $MONGODB_ROOT_USER  -p $MONGODB_ROOT_PASSWORD \
     && rm -rf /tmp/*.gz ' > ./backup/restore_mongo.log 2>&1
+
+kubectl exec -n mongodb mongodb-0 -- \
+	bash -c 'mongodump --gzip --archive -u $MONGODB_ROOT_USER  -p $MONGODB_ROOT_PASSWORD' > ./backup/dump_`date "+%Y-%m-%d_%H-%M-%S"`.gz ./backup/restore_mongo.log 2> ./backup/backup_mongo.log
