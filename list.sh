70b302e2480ae9cd654a4f13177dde6e901e632b
ArtemOnishchuk <belarys1@mail.ru>
2023-12-14 15:15:21 +0300

    Create list.sh

---
+++ list.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Get a list of files starting with "backup_minio"
+files=(backup_minio*)
+
+# Check if there are any files
+if [ ${#files[@]} -eq 0 ]; then
+    echo "No files found."
+    exit 1
+fi
+
+# Display the list of files with their numbers
+echo "Choose a file:"
+for ((i=0; i<${#files[@]}; i++)); do
+    echo "$i: ${files[i]}"
+done
+
+# Read the selected file number
+read -p "Enter the file number: " file_number
+
+# Check the validity of the file number
+if ! [[ "$file_number" =~ ^[0-9]+$ ]] || [ $file_number -ge ${#files[@]} ]; then
+    echo "Invalid file number."
+    exit 1
+fi
+
+# Use the selected file
+selected_file="${files[file_number]}"
+echo "Selected file: $selected_file"
